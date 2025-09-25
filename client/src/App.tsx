fccc6c9e2f1bdc46b713bc159714272c92528532
Kevin Winter <kcgwinter@gmail.com>
2025-09-25 11:56:00 -0400

    Add logic to fetch data from API running on localhost:5001 (api server)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+     fetch('https://localhost:5001/api/activities').then(response => response.json()).then(data => setActivities(data))
+  }, [])
 
   return (
     <>
-      <h3>Centivities</h3>
+      <h3 className="app" style={{ color: 'red'}}>Welcome to the Centivities</h3>
+      <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+      </ul>
     </>
   )
 }
+
+
 
 export default App
