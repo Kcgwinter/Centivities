b56b8a9ec60e433d899e64f07bb4d917e73082af
Kevin Winter <kcgwinter@gmail.com>
2025-09-24 20:41:52 -0400

    Create EF Tools SeedData

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7b3f5516b38ce9bd322f8f6de8489bb531fb26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b38400f737921e969b65ab586611c05b9b6b84f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
