a42c4e12bb5b54ceceb79f9aee5fd1af770f4051
Kevin Winter <kcgwinter@gmail.com>
2025-09-24 20:49:06 -0400

    create EF Database

--- Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b38400f737921e969b65ab586611c05b9b6b84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f06f13b07bad7be93cdaf3ca20a021c112e3db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
