63f06f13b07bad7be93cdaf3ca20a021c112e3db
Kevin Winter <kcgwinter@gmail.com>
2025-09-24 20:47:49 -0400

    implement auto migration

--- API/Program.cs
+++ API/Program.cs
@@ -14,4 +14,20 @@ var app = builder.Build();
 
 app.MapControllers();
 
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DBInitalizer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
 app.Run();
