a3fd70bf62d6e34e399a063f82ae5997338dd88e
Kevin Winter <kcgwinter@gmail.com>
2025-09-25 21:00:02 -0400

    Add Create Acitivity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Comands;
 using Application.Activities.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +16,12 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query{ Id = id});
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
 
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
     }
 }
