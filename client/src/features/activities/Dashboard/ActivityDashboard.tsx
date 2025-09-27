8566ac14bde746b53b6ffa824ef266ea6fbde4d1
Kevin Winter <kcgwinter@gmail.com>
2025-09-26 21:45:35 -0400

    Add Edit Form

--- client/src/features/activities/Dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/Dashboard/ActivityDashboard.tsx
@@ -1,22 +1,25 @@
-import { Grid, List} from "@mui/material";
+import { Grid, List } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[]
 }
 
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({ activities }: Props) {
     return (
         <Grid container spacing={2}>
             <Grid size={8}>
                 <List>
-                    <ActivityList activities={activities}/>
+                    <ActivityList activities={activities} />
                 </List>
             </Grid>
             <Grid size={4}>
                 {activities[0] && <ActivityDetails activity={activities[0]} />}
+
+                <ActivityForm></ActivityForm>
             </Grid>
         </Grid>
     )
