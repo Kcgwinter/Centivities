4aada426993ca6fed6f674d6a198cfc5b138c5e0
Kevin Winter <kcgwinter@gmail.com>
2025-11-03 22:33:22 -0500

    add dto

---
+++ Application/Activities/DTOs/CreateActivityDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Activities.DTOs;
+
+public class CreateActivityDto
+{
+    [Required]
+    public string Title { get; set; } = "";
+    public DateTime Date { get; set; }
+    [Required]
+    public string Description { get; set; } = "";
+    [Required]
+    public string Category { get; set; } = "";
+    [Required]
+    public string City { get; set; } = "";
+    [Required]
+    public string Venue { get; set; } = "";
+    [Required]
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
