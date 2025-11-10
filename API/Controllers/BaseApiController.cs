4bfe1890425f790e81b23c398e246192218b4e09
Kevin Winter <kcgwinter@gmail.com>
2025-11-10 10:56:42 -0500

    Fix error cant open GetDetails

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -16,9 +16,8 @@ namespace API.Controllers
 
         protected ActionResult HandleResult<T>(Result<T> result)
         {
-
             if (!result.IsSuccess && result.Code == 404) return NotFound();
-            if (!result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
 
             return BadRequest(result.Error);
         }
