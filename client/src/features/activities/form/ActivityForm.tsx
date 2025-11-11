48e3fc6d1a51e65b677ce9b3f4bfc5f49706b0a9
Kevin Winter <kcgwinter@gmail.com>
2025-11-11 14:49:30 -0500

    Add Custom Inputs

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,23 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useParams } from "react-router";
 import { useForm, type FieldValues } from 'react-hook-form';
 import { useEffect } from "react";
 import { zodResolver } from '@hookform/resolvers/zod';
+import type { Resolver } from 'react-hook-form';
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
+import TextInput from "../../../app/shared/components/TextInput";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
+import DateTimeInput from "../../../app/shared/components/DateTimeInput";
 
 export default function ActivityForm() {
 
-    const { register, reset, handleSubmit, formState: { errors } } = useForm<ActivitySchema>(
-        { resolver: zodResolver(activitySchema) }
+    const { control, reset, handleSubmit } = useForm<ActivitySchema>(
+        {
+            mode: "onTouched",
+            resolver: zodResolver(activitySchema) as Resolver<ActivitySchema>
+        }
     );
     const { id, isLoadingActivity } = useParams()
     const { updateActivitiy, createActivity, activity } = useActivities(id);
@@ -28,12 +36,12 @@ export default function ActivityForm() {
         <Paper sx={{ borderRadius: 3, padding: 3 }}>
             {activity ? <Typography variant="h5" gutterBottom color="primary">Edit Activity</Typography> : <Typography variant="h5" gutterBottom color="primary">Create Activity</Typography>}
             <Box component={"form"} onSubmit={handleSubmit(onSubmit)} display={"flex"} flexDirection={"column"} gap={3}>
-                <TextField {...register('title')} error={!!errors.title} helperText={errors.title?.message} label={"Title"} defaultValue={activity?.title} />
-                <TextField {...register('description')} error={!!errors.description} label={"Description"} multiline rows={3} defaultValue={activity?.description} />
-                <TextField {...register('category')} label={"Category"} defaultValue={activity?.category} />
-                <TextField {...register('date')} label={"Date"} type="date" defaultValue={activity?.date ? new Date(activity.date).toISOString().split('T')[0] : new Date().toISOString().split('T')[0]} />
-                <TextField {...register('city')} label={"City"} defaultValue={activity?.city} />
-                <TextField  {...register('venue')} label={"Venue"} defaultValue={activity?.venue} />
+                <TextInput label='Title' control={control} name='title' />
+                <TextInput label='Description' control={control} name='description' multiline rows={3} />
+                <SelectInput label='Category' control={control} name='category' items={categoryOptions} />
+                <DateTimeInput label='Date' control={control} name='date' />
+                <TextInput label='City' control={control} name='city' />
+                <TextInput label='Venue' control={control} name='venue' />
 
                 <Box display={"flex"} justifyContent={"end"} gap={3}>
                     <Button color="inherit">Cancel</Button>
--- client/src/lib/schemas/activitySchema.ts
+++ client/src/lib/schemas/activitySchema.ts
@@ -1,9 +1,21 @@
 import { z } from 'zod';
 
 
-const requiredString = (fieldName: string) => z.string().min(1, {message:`${fieldName} is required`})
+const requiredString = (fieldName: string) => z
+    .string()
+    // .string({ required_error: `${fieldName} is required` })
+    .min(1,
+        { message: `${fieldName} is required` })
+
 export const activitySchema = z.object({
     title: requiredString("Title"),
+    description: requiredString("Description"),
+    category: requiredString("Category"),
+    date: z.coerce.date({
+        message: 'Date is required'
+    }),
+    city: requiredString("City"),
+    venue: requiredString("Venue"),
 })
 
 export type ActivitySchema = z.infer<typeof activitySchema>;
